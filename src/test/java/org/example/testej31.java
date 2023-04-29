921ecbf0ee4c9c1234408723a99e63ccd01a5d89
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2023-04-29 20:45:04 +0200

    Initial commit

---
+++ src/test/java/org/example/testej31.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+public class testej31 {
+    //creamos un test para la clase ej31
+    @Test
+    public void testSumanTerminosProgresionGeomConNPositivoa() {
+        int resultado = main.java.org.example.ej31.sumanterminosprogresiongeom(4);
+        assertEquals(15, resultado);
+    }
+
+
+
+
+}
