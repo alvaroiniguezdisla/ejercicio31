ced37d75f072a2622c8305ad3d2cf7d0eca4502c
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2023-04-29 20:45:38 +0200

    Ejercicio 31 terminado

--- src/main/java/org/example/ej31.java
+++ src/main/java/org/example/ej31.java
@@ -1,2 +1,25 @@
-package org.example;public class ej31 {
+package org.example;
+import java.util.ArrayList;
+
+public class ej31 {
+    public static int sumanterminosprogresiongeom(int n) throws IllegalArgumentException {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n no puede ser menor o igual que 0");
+        }
+
+        int suma = 1;
+        ArrayList lista = new ArrayList();
+        for (int i = 1; i <= n; i++) {
+            suma =suma*2;
+            lista.add(suma);
+
+
+        }
+        //metodo que suma los elementos de la lista
+        int sumalista = 0;
+        for (int i = 0; i < lista.size(); i++) {
+            sumalista +=(int) lista.get(i);
+        }
+        return sumalista;
+    }
 }
--- src/test/java/org/example/testej31.java
+++ src/test/java/org/example/testej31.java
@@ -1,12 +1,25 @@
+package org.example;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 public class testej31 {
     //creamos un test para la clase ej31
-    @Test
-    public void testSumanTerminosProgresionGeomConNPositivoa() {
-        int resultado = main.java.org.example.ej31.sumanterminosprogresiongeom(4);
-        assertEquals(15, resultado);
-    }
 
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    //creamos un test para sumanterminosprogresiongeom0
+    void Testsumanterminosprogresiongeom0() {
+        assertEquals(1,ej31.sumanterminosprogresiongeom(0));
+    }
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    //creamos un test para sumanterminosprogresiongeom-1
+    void Testsumanterminosprogresiongeom1() {
+        assertEquals(1,ej31.sumanterminosprogresiongeom(-1));
+    }
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    //creamos un test para sumanterminosprogresiongeom-2
+    void Testsumanterminosprogresiongeom2() {
+        assertEquals(1,ej31.sumanterminosprogresiongeom(-2));
+    }
 
 
 
